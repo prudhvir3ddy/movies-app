cdec50722e45a3bc88a927e5cc05b30e5dbae51c
prudhvir3ddy <prudhvi.reddy@deliveroo.com>
2024-09-29 16:06:44 +0530

    movies app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, {
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDVmOGMyYWNjZWJmZmM2MTZjY2YwMzIwMjU0NzAzNyIsIm5iZiI6MTcyNzYwMzI1OS4zNzE0OTMsInN1YiI6IjVlYmZmODg0NmQ2NzVhMDAyMTdlYTRiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LdB9SeM3CFM8mGwAaIdEztMIPWHSqi57dzhayCGSFjM',
+                'accept': 'application/json',
+            }
+        })
+            .then(response => {
+                const newMovies = response.data.results;
+                movies.push(...newMovies);
+                setMovies([...movies]);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, [page]);
+
+    return (
+        <>
+            <div style={containerStyle}>
+                {movies && movies.map((movie: any) => (
+                    <div style={itemStyle} key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt=""/>
+                        <div>
+                            <h2>{movie.title}</h2>
+                            <p>{movie.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button onClick={() => setPage(page + 1)}>Load more</button>
+        </>
+    )
 }
+
+const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+};
+
+const itemStyle = {
+    flex: '1 1 30%',
+    boxSizing: 'border-box',
+    padding: '10px',
+};
 
 export default App
